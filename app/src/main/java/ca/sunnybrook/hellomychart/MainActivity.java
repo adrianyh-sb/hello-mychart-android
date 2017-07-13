a5c53d10d92533587f72e44b6313e0f8989cc7a7
ayounghoon <adrian.young-hoon@sunnybrook.ca>
2017-07-13 15:05:30 -0400

    Commented println

---
+++ app/src/main/java/ca/sunnybrook/hellomychart/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.sunnybrook.hellomychart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //println("Push to GitHub");
+    }
+}
